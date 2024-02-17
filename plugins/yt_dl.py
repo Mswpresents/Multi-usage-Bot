59514c1f53d91b28026eda6bf9f4df70dbf8632e
Ankush Yadav <138442615+Mswpresents@users.noreply.github.com>
2024-02-17 11:59:34 +0530

    Update yt_dl.py

--- plugins/yt_dl.py
+++ plugins/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [Mᴋɴ Bᴏᴛᴢ™](https://t.me/mkn_bots_updates)**"
+        cap = "**BY›› [𝕄𝕤ᴡ ℙʀᴇ𝕤ᴇɴᴛ𝕤™](https://t.me/mswpresent)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
